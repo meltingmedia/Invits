a076d1997f45ea085c115d79df9bbea03d30d820
Romain <romain@meltingmedia.net>
2012-01-11 12:44:22 +0100

    Some slight fixes

--- core/components/invits/elements/snippets/snippet.invitsConfirmRegister.php
+++ core/components/invits/elements/snippets/snippet.invitsConfirmRegister.php
@@ -16,40 +16,45 @@
 $Invits = $modx->getService('invits', 'Invits', $modx->getOption('invits.core_path', null, $modx->getOption('core_path').'components/invits/').'model/invits/', $scriptProperties);
 if (!($Invits instanceof Invits)) return '';
 
-$invitHash = $_REQUEST['referer'];
-$invit = false;
-/** @var $invit Invit */
-if ($invitHash) {
-    $invit = $modx->getObject('Invit', array(
-        'hash' => $invitHash,
-        'guest_registered' => 0,
+if ($_REQUEST['invit']) {
+    $invitHash = $_REQUEST['referer'];
+    $invit = false;
+    /** @var $invit Invit */
+    if ($invitHash) {
+        $invit = $modx->getObject('Invit', array(
+            'hash' => $invitHash,
+            'guest_registered' => 0,
+        ));
+    }
+
+    $username = base64_decode(urldecode(rawurldecode($_REQUEST['lu'])));
+    /** @var $user modUser */
+    $user = $modx->getObject('modUser', array(
+        'username' => $username,
     ));
-}
+    if (!$invitHash || !$username || !$user || !$scriptProperties['finalLanding']) return;
 
-$username = base64_decode(urldecode(rawurldecode($_REQUEST['lu'])));
-/** @var $user modUser */
-$user = $modx->getObject('modUser', array(
-    'username' => $username,
-));
-
-/**
- * $redirectParams Parameters array to be used in ConfirmRegister snippet
- *
- * $redirectParams['referer'] = invit code/hash
- * $redirectParams['landing'] = resource where the user should end
- * $redirectParams['ru'] = (invitee) user id
- */
-$redirectParams = array(
-    'referer' => $invitHash,
-    'landing' => $scriptProperties['finalLanding'],
-    'ru' => $user->get('id'),
-);
-if ($invit) {
-    $scriptProperties['redirectParams'] = json_encode($redirectParams);
-    if (isset($scriptProperties['invitRemove'])) {
-        $scriptProperties['redirectTo'] = $scriptProperties['invitRemove'];
+    /**
+     * $redirectParams Parameters array to be used in ConfirmRegister snippet
+     *
+     * $redirectParams['referer'] = invit code/hash
+     * $redirectParams['landing'] = resource where the user should end
+     * $redirectParams['ru'] = (invitee, registered user) user id
+     */
+    $redirectParams = array(
+        'referer' => $invitHash,
+        'landing' => $scriptProperties['finalLanding'],
+        'ru' => $user->get('id'),
+    );
+    $redirectParams = array_merge($scriptProperties['redirectParams'], $redirectParams);
+    if ($invit) {
+        $scriptProperties['redirectParams'] = json_encode($redirectParams);
+        if (isset($scriptProperties['invitRemove'])) {
+            $scriptProperties['redirectTo'] = $scriptProperties['invitRemove'];
+        }
     }
 }
 // @todo: unset invitsConfirmRegister params ? (like invitRemove, finalLanding)
 $modx->runSnippet('ConfirmRegister', $scriptProperties);
-return 'you should not see this!';
+
+return;
